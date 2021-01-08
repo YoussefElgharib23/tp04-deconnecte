d565a34fdf5ced30a09b54cc0a36f3484ab0f09c
YoussefElgharib23 <youssef.elgharib17@gmail.com>
2021-01-08 15:37:26 +0100

    First commit to the project

---
+++ TP4-ModeDeconnecte/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace TP4_ModeDeconnecte
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxPrenom = new System.Windows.Forms.TextBox();
+            this.DateNaissance = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxSerivce = new System.Windows.Forms.ComboBox();
+            this.dataGridViewEmpl = new System.Windows.Forms.DataGridView();
+            this.btnChercher = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBoxServAfficher = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(178, 58);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(232, 22);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gestion des employes";
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(178, 86);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(232, 22);
+            this.textBoxNom.TabIndex = 0;
+            // 
+            // textBoxPrenom
+            // 
+            this.textBoxPrenom.Location = new System.Drawing.Point(178, 114);
+            this.textBoxPrenom.Name = "textBoxPrenom";
+            this.textBoxPrenom.Size = new System.Drawing.Size(232, 22);
+            this.textBoxPrenom.TabIndex = 0;
+            // 
+            // DateNaissance
+            // 
+            this.DateNaissance.Location = new System.Drawing.Point(178, 142);
+            this.DateNaissance.Name = "DateNaissance";
+            this.DateNaissance.Size = new System.Drawing.Size(232, 22);
+            this.DateNaissance.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nom:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Prenom:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Date de naissance:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Service:";
+            // 
+            // comboBoxSerivce
+            // 
+            this.comboBoxSerivce.FormattingEnabled = true;
+            this.comboBoxSerivce.Location = new System.Drawing.Point(178, 173);
+            this.comboBoxSerivce.Name = "comboBoxSerivce";
+            this.comboBoxSerivce.Size = new System.Drawing.Size(232, 24);
+            this.comboBoxSerivce.TabIndex = 5;
+            // 
+            // dataGridViewEmpl
+            // 
+            this.dataGridViewEmpl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpl.Location = new System.Drawing.Point(10, 303);
+            this.dataGridViewEmpl.Name = "dataGridViewEmpl";
+            this.dataGridViewEmpl.RowHeadersWidth = 51;
+            this.dataGridViewEmpl.RowTemplate.Height = 24;
+            this.dataGridViewEmpl.Size = new System.Drawing.Size(600, 237);
+            this.dataGridViewEmpl.TabIndex = 6;
+            // 
+            // btnChercher
+            // 
+            this.btnChercher.Location = new System.Drawing.Point(434, 54);
+            this.btnChercher.Name = "btnChercher";
+            this.btnChercher.Size = new System.Drawing.Size(97, 30);
+            this.btnChercher.TabIndex = 7;
+            this.btnChercher.Text = "Chercher";
+            this.btnChercher.UseVisualStyleBackColor = true;
+            this.btnChercher.Click += new System.EventHandler(this.btnChercher_Click);
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(112, 213);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(90, 30);
+            this.btnAjouter.TabIndex = 7;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(231, 213);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(90, 30);
+            this.btnModifier.TabIndex = 7;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(355, 213);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(90, 30);
+            this.btnSupprimer.TabIndex = 7;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 17);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Service:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(351, 255);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 30);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Afficher";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBoxServAfficher
+            // 
+            this.textBoxServAfficher.Location = new System.Drawing.Point(112, 259);
+            this.textBoxServAfficher.Name = "textBoxServAfficher";
+            this.textBoxServAfficher.Size = new System.Drawing.Size(222, 22);
+            this.textBoxServAfficher.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 562);
+            this.Controls.Add(this.textBoxServAfficher);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnChercher);
+            this.Controls.Add(this.dataGridViewEmpl);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBoxSerivce);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DateNaissance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrenom);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.textBoxId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.TextBox textBoxPrenom;
+        private System.Windows.Forms.DateTimePicker DateNaissance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxSerivce;
+        private System.Windows.Forms.DataGridView dataGridViewEmpl;
+        private System.Windows.Forms.Button btnChercher;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBoxServAfficher;
+    }
+}
+
---
+++ TP4-ModeDeconnecte/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TP4_ModeDeconnecte
+{
+    public partial class Form1 : Form
+    {
+        static SqlConnection connection = new SqlConnection(@"Data Source=localhost;Initial Catalog=TP_01_Stagiaires;Integrated Security=True");
+        SqlDataAdapter employeAd = new SqlDataAdapter("SELECT * FROM Employe", connection);
+        SqlDataAdapter serviceAd = new SqlDataAdapter("SELECT * FROM Service", connection);
+        DataSet data = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            employeAd.Fill(data, "Employe");
+            serviceAd.Fill(data, "Service");
+            comboBoxSerivce.DataSource = null;
+            comboBoxSerivce.DataSource = data.Tables["Service"];
+            comboBoxSerivce.DisplayMember = "nomServ";
+            comboBoxSerivce.ValueMember = "IdService";
+            remplirDataGridView();
+        }
+        
+        private void remplirDataGridView()
+        {
+            dataGridViewEmpl.DataSource = null;
+            dataGridViewEmpl.DataSource = data.Tables["Employe"];
+        }
+
+        private void btnChercher_Click(object sender, EventArgs e)
+        {
+            if (!Exist())
+            {
+                MessageBox.Show("L'employe n'existe pas");
+                return;
+            }
+            foreach (DataRow row in data.Tables["Employe"].Rows)
+            {
+                if (textBoxId.Text == row["id"].ToString())
+                {
+                    textBoxNom.Text = row["nom"].ToString();
+                    textBoxPrenom.Text = row["prenom"].ToString();
+                    DateNaissance.Value = DateTime.Parse(row["dateNaissance"].ToString());
+                    comboBoxSerivce.SelectedValue = Int32.Parse(row["Id_Service"].ToString());
+                    break;
+                }
+            }
+            
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            if (Exist())
+            {
+                MessageBox.Show("L'employe existe dèja");
+            }
+            else
+            {
+                DataRow row = data.Tables["Employe"].NewRow();
+                row["id"] = Int32.Parse(textBoxId.Text);
+                row["nom"] = textBoxNom.Text;
+                row["prenom"] = textBoxPrenom.Text;
+                row["dateNaissance"] = DateNaissance.Value.ToShortDateString();
+                row["Id_Service"] = comboBoxSerivce.SelectedValue;
+                data.Tables["Employe"].Rows.Add(row);
+                SqlCommandBuilder builder = new SqlCommandBuilder(employeAd);
+                builder.GetInsertCommand();
+                employeAd.Update(data, "Employe");
+                remplirDataGridView();
+                MessageBox.Show("l'employe est enregistré");
+            }
+        }
+
+        private bool Exist()
+        {
+            foreach (DataRow row in data.Tables["Employe"].Rows)
+            {
+                if (textBoxId.Text == row["id"].ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            if (Exist())
+            {
+                foreach (DataRow row in data.Tables["Employe"].Rows)
+                {
+                    if (textBoxId.Text == row["id"].ToString())
+                    {
+                        row["nom"] = textBoxNom.Text;
+                        row["prenom"] = textBoxPrenom.Text;
+                        row["dateNaissance"] = DateNaissance.Value.ToShortDateString();
+                        row["Id_Service"] = comboBoxSerivce.SelectedValue;
+                        SqlCommandBuilder builder = new SqlCommandBuilder(employeAd);
+                        builder.GetInsertCommand();
+                        employeAd.Update(data, "Employe");
+                        remplirDataGridView();
+                        MessageBox.Show("l'employe est modifié");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("l'employe n'existe pas");
+            }
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            if (Exist())
+            {
+                foreach (DataRow row in data.Tables["Employe"].Rows)
+                {
+                    if (textBoxId.Text == row["id"].ToString())
+                    {
+                        row.Delete();
+                        SqlCommandBuilder builder = new SqlCommandBuilder(employeAd);
+                        builder.GetInsertCommand();
+                        employeAd.Update(data, "Employe");
+                        remplirDataGridView();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("l'employe n'existe pas");
+            }
+        }
+    }
+}
